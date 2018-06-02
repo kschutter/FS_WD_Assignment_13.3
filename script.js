1d5421745895050cbf7e19f4745a39ca49f1dc33
Kyle Schutter <kmosswork@gmail.com>
2018-06-01 20:59:51 -0600

    Initialized then completed the assignment

---
+++ script.js
@@ -0,0 +1,22 @@
+var student = {
+    // vars
+    "name": "Kyle",
+    "age": 21,
+    "weight": 200,
+    "height": 72.5,
+
+    // Array
+    "friends": ["Ashley", "Ally", "David"],
+
+    // Object
+    "college": {
+        "name": "Miami University",
+        "city": "Oxford",
+        "zipcode": 45056
+    }
+}
+
+// Test cases
+console.log(student.name);
+console.log(student.friends[0]);
+console.log(student.college.city);
